b2aada24156a06c13a3f05204e9e59fde4b66d22
Hassan-code1 <2024kuec2011@iiitkota.ac.in>
2025-07-24 21:08:26 +0530

    register/login control

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const authRoutes = require("./routes/auth");
+app.use("/api/auth", authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
